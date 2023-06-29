d56fd8bdc906b627405b7573af4d2a3259c06e5e
JosHenryK <119651219+JosHenryK@users.noreply.github.com>
2023-06-29 16:21:05 -0400

    test

--- simulation/constants.py
+++ simulation/constants.py
@@ -1,6 +1,7 @@
 resistance = 0 #resistance of the RTP crystal
 capacitance = 0 #capacitance of the RTP crystal
 nominal_voltage = 0 #nominal voltage of the power circuitry
+nominal_frequency = 0 #nominal frequency of the power circuitry
 voltage_ripple = 0 #voltage ripple of the power circuitry
 switching_frequency = 0 #switching frequency of the power circuitry
 
