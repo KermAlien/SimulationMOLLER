da40594e50e66ddb3986a6d1c8cf818318a4f094
JosHenryK <119651219+JosHenryK@users.noreply.github.com>
2023-06-29 15:16:52 -0400

    added deadtime calculation

--- simulation/translationLayer.py
+++ simulation/translationLayer.py
@@ -2,11 +2,15 @@ import math
 from math import pi
 import constants
 
+current_limit = constants.nominal_voltage / constants.resistance
+beta = 1 / current_limit
+deadtime = beta * constants.capacitance * constants.nominal_voltage
+
 nominal_voltage = constants.nominal_voltage #5 #nominal voltage of the wave
 transient_voltage = 0 #10 #transient voltage of the wave
 voltage_ripple = constants.voltage_ripple #0.5 #voltage ripple in the wave at nominal voltage
 nominal_frequency = 0 #3 #wave frequency
-transient_frequency = 0 #1 #transient frequency
+transient_frequency = 1 / deadtime #1 #transient frequency
 switching_frequency = constants.switching_frequency #2 #switching frequency
 time_constant = constants.resistance * constants.capacitance #0.75 #arbitrary value to set the agression of the transient decay, zero equals no decay and one equals immediate decay
 
