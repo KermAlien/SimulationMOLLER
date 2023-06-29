073aa35deab69db2c282fdf32153d349ac4777cc
JosHenryK <119651219+JosHenryK@users.noreply.github.com>
2023-06-29 10:47:07 -0400

    added decay function

--- transientSimulation.py
+++ transientSimulation.py
@@ -10,13 +10,14 @@ import time
 nominal_voltage = 5 #nominal voltage of the wave
 transient_voltage = 10 #transient voltage of the wave
 voltage_ripple = 0.5 #voltage ripple in the wave at nominal voltage
-nominal_frequency = 2 #wave frequency
+nominal_frequency = 3 #wave frequency
 transient_frequency = 1 #transient frequency
 switching_frequency = 2 #switching frequency
+aggression = 0.7 #arbitrary value to set the agression of the transient decay, zero equals no decay and one equals immediate decay
 
 num_of_phases = 3 #number of phases per wave module
 num_of_wave_modules = 3 #number of wave modules
-increment_resolution = 0.001 #resolution with which the wave module is generated, measured in radians
+increment_resolution = 0.01 #resolution with which the wave module is generated, measured in radians
 
 storage = [] #list used for graph generation
 nominal_angular_frequency = nominal_frequency * (2 * pi) #calculate the period of the wave in radians
@@ -25,6 +26,13 @@ switching_angular_frequency = switching_frequency * (2 * pi) #calculate the peri
 
 nominal_period = 1 / nominal_frequency
 transient_period = 1 / transient_frequency
+
+def calc_transient_decay(time): #calculate the decay of the transient according to a decay function
+    decay_amplitude = transient_voltage * pow((1 - aggression) , time) #decay funtion, transient_voltage * (1-aggression)^x
+    if (decay_amplitude > voltage_ripple):
+        return decay_amplitude
+    else: 
+        return voltage_ripple
 
 def calc_wave_amplitude(amplitude , frequency , time): #calculate the amplitude of the wave at a given time measured in radians
     current_amplitude = amplitude * math.cos(frequency * time)
@@ -40,7 +48,7 @@ def calc_wave_module(offset , polarity): #iterate calculating the current wave a
                 time = time + increment_resolution
         else:
             while (time < nominal_angular_frequency):
-                storage.append(calc_wave_amplitude(voltage_ripple , nominal_frequency, time) + offset)  
+                storage.append(calc_wave_amplitude(calc_transient_decay(int_num_of_phases) , nominal_frequency, time) + offset)  
                 time = time + increment_resolution   
         int_num_of_phases = int_num_of_phases + 1
 
