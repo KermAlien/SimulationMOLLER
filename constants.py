29ab2d44373cf7e2ae19bbf18810c447c73bc4f0
Sam Gibbs <73617113+KermAlien@users.noreply.github.com>
2023-06-29 14:53:22 -0400

    Changes

--- constants.py
+++ constants.py
@@ -1,4 +1,31 @@
-# Constant variables (customize as needed)
-beam_energy = 11.0  # Beam energy in GeV
-beam_polarization = 0.9  # Beam polarization value
-beam_current = 65.0  # Beam current in microamps
+# Deadtime constants
+resistance = 5 # Measured in Megaohms ()
+capacitance = 3 # Measured in picofarads (2-3 when 12x12x10)
+voltage = 100 # Measured in kilovolts ()
+currentLimit = voltage/resistance # Measured in kiloamperes
+
+beta = 1/currentLimit
+
+deadtime = beta * capacitance * voltage
+
+
+
+
+
+
+
+
+
+
+driverFrequency = 1.92 # Measured in kilohertz
+
+# Want to minimize aperture size
+# Longer crystal = less V, more C
+# Deadtime limited by current limit through optocouplers
+# With solid-states drivers deadtime can be significantly reduced but peizoeletric ringing becomes an issue
+
+# 12x12x10 mm crystals
+# When deadtime < 7-8 microseconds some ringing is present
+# Larger crystals = easier ringing
+
+frequencyResonance = 250 # Measured in kilohertz (200-350)
--- main.py
+++
@@ -1,3 +0,0 @@
-import beamGeneration
-
-beamGeneration.simulate_beam()
