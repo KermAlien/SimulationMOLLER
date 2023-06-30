33d5d9127a8f27a7ef9c5b284606cdd9a981abd2
JosHenryK <119651219+JosHenryK@users.noreply.github.com>
2023-06-30 08:24:06 -0400

    working demonstration version

--- simulation/Demonstration.py
+++ simulation/Demonstration.py
@@ -7,12 +7,12 @@ nominal_voltage = 5 #nominal voltage of the wave
 transient_voltage = 10 #transient voltage of the wave
 voltage_ripple = 0.5 #voltage ripple in the wave at nominal voltage
 nominal_frequency = 3 #wave frequency
-transient_frequency = 1 #transient frequency
+transient_frequency = 0.2 #transient frequency
 switching_frequency = 2 #switching frequency
 aggression = 0.7 #arbitrary value to set the agression of the transient decay, zero equals no decay and one equals immediate decay
 aggression = 0.75 #arbitrary value to set the agression of the transient decay, zero equals no decay and one equals immediate decay
 
-num_of_phases = 3 #number of phases per wave module
+num_of_phases = 5 #number of phases per wave module
 num_of_wave_modules = 3 #number of wave modules
 increment_resolution = 0.01 #resolution with which the wave module is generated, measured in radians
 
