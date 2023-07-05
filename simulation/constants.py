b874b19c59998a1bc154198f441404c0e05e3693
JosHenryK <119651219+JosHenryK@users.noreply.github.com>
2023-07-05 15:06:54 -0400

    added temporary variables in constants

--- simulation/constants.py
+++ simulation/constants.py
@@ -1,10 +1,14 @@
 resistance = 5000000 #resistance of the RTP crystal, measured in ohms
 capacitance = 0.000000000002 #capacitance of the RTP crystal, measured in farads
-nominal_voltage = 3000 #nominal voltage of the power circuitry, meansured in volts
-nominal_frequency = 1920 #nominal frequency of the power circuitry, measured in hertz
-voltage_ripple = 5 #voltage ripple of the power circuitry, measured in volts
+nominal_voltage = 100 #nominal voltage of the power circuitry, meansured in volts
+nominal_frequency = 50 #1920 #nominal frequency of the power circuitry, measured in hertz
+voltage_ripple = 10 #voltage ripple of the power circuitry, measured in volts
 switching_frequency = 10 #switching frequency of the power circuitry, measured in hertz
 
 num_of_modules = 3 #number of simulated voltage transistions
 resolution = 0.01 #resolution with which the wave is generated, measured in radians
-time_resolution = 0.75 #resolution for the x-axis on the graph, measured in seconds
+time_resolution = 0.25 #resolution for the x-axis on the graph, measured in seconds
+
+#temp:
+transient_voltage = 0
+rise_time = 0
