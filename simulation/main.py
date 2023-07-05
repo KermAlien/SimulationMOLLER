0783479ad39792a2072891ab48dbc68a50da1540
JosHenryK <119651219+JosHenryK@users.noreply.github.com>
2023-07-05 14:34:18 -0400

    refactored x-axis to seconds instead of radians

--- simulation/main.py
+++ simulation/main.py
@@ -1,7 +1,21 @@
+import constants
+import translationLayer
 import transientSimulation
 import matplotlib.pyplot as plt
 
 transientSimulation.calc_wave()
 
 plt.plot(transientSimulation.storage)
+
+tick_location = []
+tick_label = []
+
+num_of_seconds = (len(transientSimulation.storage) * constants.resolution) / translationLayer.nominal_angular_frequency
+int_num_of_seconds = 0
+while (int_num_of_seconds < (num_of_seconds + 1)):
+    tick_location.append(int_num_of_seconds * (len(transientSimulation.storage) / num_of_seconds))
+    tick_label.append(int_num_of_seconds)
+    int_num_of_seconds = int_num_of_seconds + 1
+
+plt.xticks(tick_location , tick_label)
 plt.show()
