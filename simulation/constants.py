da56184099b07d11abcba195cb441558b15b7169
JosHenryK <119651219+JosHenryK@users.noreply.github.com>
2023-07-06 15:36:35 -0400

    updated triggerPulse constant variables

--- simulation/constants.py
+++ simulation/constants.py
@@ -7,19 +7,19 @@ voltage_ripple = 5 #voltage ripple of the power circuitry, measured in volts
 switching_frequency = 25 #1920 #switching frequency of the power circuitry, measured in hertz
 
 #trigger pulse:
-trigger_duty_cycle = 0
-trigger_nominal_voltage = 0
+trigger_duty_cycle = 0.5
+trigger_nominal_voltage = 5
 
 #systematic error:
 percent_error = 0
 
 #general:
-num_of_modules = 1 #number of simulated voltage transistions
-generation_resolution = 0.01 #resolution with which the wave is generated, measured in radians
+num_of_modules = 3 #number of simulated voltage transistions
+generation_resolution = 0.001 #resolution with which the wave is generated, measured in radians
 time_resolution = 0.25 #resolution for the x-axis on the graph, measured in seconds
 
 #temp:
 transient_voltage = 25
 transient_rise_time = 0.001
-trigger_rise_time = 0
+trigger_rise_time = 0.001
 trigger_latency = 0
