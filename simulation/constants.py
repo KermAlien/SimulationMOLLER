63584cfef2859621b01d395939a42e3fd0c64e87
JosHenryK <119651219+JosHenryK@users.noreply.github.com>
2023-07-06 08:35:44 -0400

    added triggerPulse file

--- simulation/constants.py
+++ simulation/constants.py
@@ -1,6 +1,7 @@
-resistance = 5000000 #resistance of the RTP crystal, measured in ohms
-capacitance = 0.000000000002 #capacitance of the RTP crystal, measured in farads
-nominal_voltage = 10 #nominal voltage of the power circuitry, meansured in volts
+#transient simulation:
+resistance = 730000000 #resistance of the RTP crystal, measured in ohms
+capacitance = 0.0000000000033 #capacitance of the RTP crystal, measured in farads
+nominal_voltage = 30 #nominal voltage of the power circuitry, meansured in volts
 nominal_frequency = 25 #1920 #nominal frequency of the power circuitry, measured in hertz
 voltage_ripple = 5 #voltage ripple of the power circuitry, measured in volts
 switching_frequency = 10 #switching frequency of the power circuitry, measured in hertz
@@ -8,6 +9,9 @@ switching_frequency = 10 #switching frequency of the power circuitry, measured i
 num_of_modules = 3 #number of simulated voltage transistions
 generation_resolution = 0.01 #resolution with which the wave is generated, measured in radians
 time_resolution = 0.25 #resolution for the x-axis on the graph, measured in seconds
+
+#trigger pulse:
+
 
 #temp:
 transient_voltage = 25
