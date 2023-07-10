90069669ffda77a1cfba08152cf39606f73ea549
JosHenryK <119651219+JosHenryK@users.noreply.github.com>
2023-07-10 16:13:25 -0400

    began changes to set_time_interval()

--- simulation/main.py
+++ simulation/main.py
@@ -11,9 +11,9 @@ graph_time_scale = translationLayer.graph_time_scale
 graph_time_interval = translationLayer.graph_time_interval
 nominal_angular_frequency = translationLayer.nominal_angular_frequency
 
-def set_time_resolution(): #sets the time resolution for the x-axis of the graph in seconds
-    tick_location = [] #list to store the x-axis tick locations on the graph
-    tick_label = [] #list to store the x-axis tick labels on the graph
+def set_time_resolution(interval): #sets the time resolution for the x-axis of the graph in seconds, parameters array in format list, resolution in radians, angular_frequency in radians / second, and interval in seconds
+    tick_location = [] #list to store the tick locations on the graph
+    tick_label = [] #list to store the tick labels on the graph
     num_of_seconds = (len(storage) * generation_resolution) / nominal_angular_frequency
     int_num_of_seconds = 0
     while (int_num_of_seconds < (num_of_seconds + graph_time_scale)):
--- simulation/translationLayer.py
+++ simulation/translationLayer.py
@@ -42,3 +42,5 @@ num_of_modules = constants.num_of_modules #number of wave modules
 generation_resolution = constants.generation_resolution #resolution with which the wave module is generated, measured in radians
 graph_time_scale = constants.graph_time_scale #resolution for the x-axis on the graph, measured in seconds
 graph_time_interval = constants.graph_time_interval #interval for the x-axis on the graph, measured in seconds
+
+#need to change frequency for angular frequency in wave calculations
