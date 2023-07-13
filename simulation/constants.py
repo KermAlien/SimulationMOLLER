65dde1486a989edd86f7d85988a7b68c9f55d80e
Sam Gibbs <73617113+KermAlien@users.noreply.github.com>
2023-07-13 09:55:06 -0400

    Original Henry + Sam Branch merge

---
+++ simulation/constants.py
@@ -0,0 +1,41 @@
+import math
+
+#transient simulation:
+resistance = 730000000000 # resistance of the RTP crystal, measured in ohms
+capacitance = 0.0000000000000006 # capacitance of the RTP crystal, measured in farads
+nominal_voltage_positve = 3000 #positve nominal voltage of the power circuitry, measured in volts
+nominal_voltage_negative = 3000 #negative nominal voltage of the power circuitry, measured in volts
+nominal_frequency_positive = 333333 #positive nominal frequency of the power circuitry, measured in hertz
+nominal_frequency_negative = 333333 #negative nominal frequency of the power circuitry, measured in hertz
+voltage_ripple_positive = 600 #voltage ripple of the power circuitry, measured in volts
+voltage_ripple_negative = 600 #voltage ripple of the power circuitry, measured in volts
+transient_voltage_positive = 14400 #voltage of the transient, measured in volts
+transient_voltage_negative = 14400 #voltage of the transient, measured in volts
+transient_rt_voltage_positive = 17798.16 #voltage of the transient, measured in volts
+transient_rt_voltage_negative = 17798.16 #voltage of the transient, measured in volts
+transient_frequency_positive = 98039 #positive frequency of the transient, measured in hertz
+transient_frequency_negative = 98039 #negative frequency of the transient, measured in hertz
+switching_frequency = 1920 #switching frequency of the power circuitry, measured in hertz
+
+#trigger pulse:
+#trigger_duty_cycle = 0.5
+#trigger_nominal_voltage = 5
+#trigger_latency = 0
+#trigger_rise_time = 0.001
+
+#systematic error:
+#percent_error = 0
+
+#general:
+transient_resolution = 0.000000001 #resolution with which the transient wave is generated, measured in radians
+nominal_resolution = math.pi / 32 #resolution with which the nominal wave is generated, measured in radians
+
+num_of_modules = 3 #number of simulated voltage transistions
+graph_time_interval = 0.0001 #interval for the x-axis on the graph, measured in seconds
+
+BCM_resolution = 1 #resolution with which the wave is "measured" by the BCM, measured in hertz
+detector_resolution = 0.01 #resolution with which the wave is "measured" by the detector, measured in hertz
+
+#timer
+timer_lower_bound = 0 #1068
+timer_upper_bound = 10000000 #1075
---
+++ simulation/main.py
@@ -0,0 +1,40 @@
+import translationLayer
+import transientSimulation
+import matplotlib.pyplot as plt
+
+transientSimulation.calc_wave()
+
+storage = transientSimulation.storage
+
+# nominal_angular_frequency_positive = translationLayer.nominal_angular_frequency_positive
+# nominal_angular_frequency_negative = translationLayer.nominal_angular_frequency_negative
+# switching_period = translationLayer.switching_period
+# num_of_modules = translationLayer.num_of_modules
+# graph_time_interval = translationLayer.graph_time_interval
+
+# def set_time_resolution(interval): #sets the time resolution for the x-axis of the graph in seconds, parameters array in format list, resolution in radians, angular_frequency in radians / second, and interval in seconds
+#     tick_location = [] #list to store the tick locations on the graph
+#     tick_label = [] #list to store the tick labels on the graph
+#     num_of_modules_positive = round(num_of_modules / 2)
+#     num_of_modules_negative = int(num_of_modules / 2)
+#     num_of_seconds_positive = (num_of_modules_positive * switching_period) + (num_of_modules_negative * transient_rise_time_positive)
+#     num_of_seconds_negative = (num_of_modules_negative * switching_period) + ((num_of_modules_positive - 1) * transient_rise_time_negative)
+#     num_of_seconds = num_of_seconds_positive + num_of_seconds_negative
+#     int_num_of_seconds = 0
+#     while (int_num_of_seconds < (num_of_seconds + interval)):
+#         tick_location.append(int_num_of_seconds * (len(storage) / num_of_seconds))
+#         num_of_decimal_places = 4
+#         rounded_label = round(int_num_of_seconds , num_of_decimal_places)
+#         tick_label.append(rounded_label)
+#         int_num_of_seconds = int_num_of_seconds + interval
+#     plt.xticks(tick_location , tick_label)
+
+# def read_current_amplitude(radian_location): #returns the current amplitude of the wave at a given radian location, parameter radian_location in radians
+#     current_amplitude = storage[radian_location / generation_resolution]
+#     return current_amplitude
+
+plt.plot(storage)
+# set_time_resolution(graph_time_interval)
+plt.show()
+
+#not sure if set_time_resolution() is setting an accurate time scale
