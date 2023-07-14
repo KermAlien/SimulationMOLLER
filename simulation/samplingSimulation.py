c5a0c827cb41e57a6f210112d290ee79ce75b1a0
Sam Gibbs <73617113+KermAlien@users.noreply.github.com>
2023-07-14 15:46:49 -0400

    Updated Merged

---
+++ simulation/samplingSimulation.py
@@ -0,0 +1,61 @@
+import translationLayer
+import transientSimulation
+
+detector_period = translationLayer.detector_period
+BCM_period = translationLayer.BCM_period
+num_of_modules = translationLayer.num_of_modules
+
+<<<<<<< Updated upstream:samplingSimulation.py
+# storage = transientSimulation.storage
+# generation_resolution = transientSimulation.generation_resolution
+=======
+storage = transientSimulation.storage
+generation_resolution = translationLayer.generation_resolution
+
+BCM_storage = [] #list used for BCM graph generation
+detector_storage = [] #list used for detector graph generation
+>>>>>>> Stashed changes:simulation/samplingSimulation.py
+
+rads_in_wave = len(storage) * generation_resolution #total number of radians in the original wave
+
+wave_period = translationLayer.switching_period * num_of_modules #period of the original wave, 0 is a PLACEHOLDER
+
+num_of_measurements_d = wave_period / detector_period #number of measurements that the detector takes of the original wave
+num_of_measurements_b = wave_period / BCM_period #number of measurements that the BCM takes of the original wave
+
+interval_d = rads_in_wave / num_of_measurements_d #interval that the detector uses to take measurements of the original wave
+interval_b = rads_in_wave / num_of_measurements_b #interval that the BCM uses to take measurements of the original wave
+
+def read_current_amplitude(radian_location): #returns the current amplitude of the wave at a given radian location, parameter radian_location in radians
+    current_amplitude = storage[int(radian_location / generation_resolution)]
+    return current_amplitude
+
+<<<<<<< Updated upstream:samplingSimulation.py
+# def calc_wave_detector():
+#     time = 0
+#     location = 0
+#     while (time < num_of_measurements_d):
+#         #mas
+#         time = time + interval_d
+#         location = location + interval_d
+
+# def calc_wave_BCM():
+#     time = 0
+#     location = 0
+#     while (time < num_of_measurements_b):
+#         #mas
+#         time = time + interval_b
+#         location = location + interval_b
+=======
+def calc_wave_detector():
+    time = 0
+    while (time < num_of_measurements_d):
+        detector_storage.append(read_current_amplitude(time))
+        time = time + interval_d
+
+def calc_wave_BCM():
+    time = 0
+    while (time < num_of_measurements_b):
+        BCM_storage.append(read_current_amplitude(time))
+        time = time + interval_b
+>>>>>>> Stashed changes:simulation/samplingSimulation.py
---
+++ simulation/translationLayer.py
@@ -0,0 +1,62 @@
+import math
+from math import pi
+import constants
+
+#transient simulation:
+resistance = constants.resistance
+capacitance = constants.capacitance
+
+nominal_voltage_positve = constants.nominal_voltage_positve #positive nominal voltage of the wave, measured in volts
+nominal_voltage_negative = constants.nominal_voltage_negative #negative nominal voltage of the wave, measured in volts
+nominal_frequency_positive = constants.nominal_frequency_positive #positive wave frequency, measured in hertz
+nominal_frequency_negative = constants.nominal_frequency_negative #negative wave frequency, measured in hertz
+voltage_ripple_positive = constants.voltage_ripple_positive #positive voltage ripple in the wave at nominal voltage, measured in volts
+voltage_ripple_negative = constants.voltage_ripple_negative #negative voltage ripple in the wave at nominal voltage, measured in volts
+transient_voltage_positive = constants.transient_voltage_positive #positive transient voltage of the wave, measured in volts
+transient_voltage_negative = constants.transient_voltage_negative #negative transient voltage of the wave, measured in volts
+transient_rt_voltage_positive = constants.transient_rt_voltage_positive #positive voltage of the rise time for the transient of the wave, measured in volts
+transient_rt_voltage_negative = constants.transient_rt_voltage_negative #negative voltage of the rise time for the transient of the wave, measured in volts
+transient_frequency_positive = constants.transient_frequency_positive #positive transient frequency, measured in hertz
+transient_frequency_negative = constants.transient_frequency_negative #negative transient frequency, measured in hertz
+switching_frequency = constants.switching_frequency #switching frequency, measured in hertz
+time_constant = resistance * capacitance #value to determine the rate of transient decay, full decay occurs after five time constants, measured in seconds
+
+nominal_period_positive = 1 / nominal_frequency_positive #positive period of the wave, measured in seconds
+nominal_period_negative = 1 / nominal_frequency_negative #negative period of the wave, measured in seconds
+transient_period_positive = 1 / transient_frequency_positive #postive period of the transient, measured in seconds
+transient_period_negative = 1 / transient_frequency_negative #negative period of the transient, measured in seconds
+switching_period = 1 / switching_frequency #period of the switching, measured in seconds
+
+nominal_angular_frequency_positive = int(nominal_frequency_positive * (2 * pi)) #positve angular frequency of the wave, measured in radians
+nominal_angular_frequency_negative = int(nominal_frequency_negative * (2 * pi)) #negative angular frequency of the wave, measured in radians
+transient_angular_frequency_positive = int(transient_frequency_positive * (2 * pi)) #positive angular frequency of the transient, measured in radians
+transient_angular_frequency_negative = int(transient_frequency_negative * (2 * pi)) #negative angular frequency of the transient, measured in radians
+switching_angular_frequency = int(switching_frequency * (2 * pi)) #angular frequency of the switching, measured in radians
+
+#trigger pulse:
+#trigger_duty_cycle = constants.trigger_duty_cycle
+#trigger_nominal_voltage = constants.trigger_nominal_voltage
+#trigger_latency = constants.trigger_latency
+
+#trigger_rise_time = constants.trigger_rise_time
+#trigger_radian_rise_time = trigger_rise_time * nominal_angular_frequency
+
+#systematic error:
+#percent_error = constants.percent_error
+
+#general:
+num_of_phases_positive = (switching_period - transient_period_positive) / nominal_period_positive #positive number of nominal phases per wave module
+num_of_phases_negative = (switching_period - transient_period_negative) / nominal_period_negative #negative number of nominal phases per wave module
+num_of_modules = constants.num_of_modules #number of wave modules
+graph_time_interval = constants.graph_time_interval #interval for the x-axis on the graph, measured in seconds
+
+generation_resolution = constants.generation_resolution
+# transient_resolution = constants.transient_resolution #resolution with which the transient wave is generated, measured in radians
+# nominal_resolution = constants.nominal_resolution #resolution with which the nominal wave is generated, measured in radians
+
+detector_period = 1 / constants.detector_resolution #period with which the wave is "measured" by the detector, measured in seconds
+BCM_period = 1 / constants.BCM_resolution #period with which the wave is "measured" by the BCM, measured in seconds
+
+#timer
+timer_lower_bound = constants.timer_lower_bound
+timer_upper_bound = constants.timer_upper_bound
