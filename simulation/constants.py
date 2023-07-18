81ce914506487e273d3dae7df142f049dc586dc2
JosHenryK <119651219+JosHenryK@users.noreply.github.com>
2023-07-18 10:52:16 -0400

    working triggerPulse generation

--- simulation/constants.py
+++ simulation/constants.py
@@ -26,4 +26,4 @@ upper_bound_limit = 0.4 #horizontal upper bound limit of the graph, measured in 
 trigger_duty_cycle = 0.5 #duty cycle of the trigger pulse
 trigger_nominal_voltage = 5 #nominal voltage of the trigger pulse 
 trigger_latency = 0 #latency of the trigger pulse ahead of the wave generation, measured in seconds
-trigger_rise_time = 0.001 #period of the trigger pulse rise time, measured in seconds
+trigger_rise_time = 0.0001 #period of the trigger pulse rise time, measured in seconds
