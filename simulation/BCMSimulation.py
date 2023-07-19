98c324dd7d1d2a6193fd97c30975e657f0bc02f9
JosHenryK <119651219+JosHenryK@users.noreply.github.com>
2023-07-19 13:02:04 -0400

    working calc_bcm with experimental offset

--- simulation/BCMSimulation.py
+++ simulation/BCMSimulation.py
@@ -11,6 +11,6 @@ def calc_bcm():
     num_of_calcs = num_of_seconds_bounded * bcm_sampling_rate
     bcm_resolution = num_of_radians_bounded / num_of_calcs
     time = 0
-    while(time < num_of_radians_bounded):
+    while(time < num_of_radians_bounded - (0.12 * num_of_radians_bounded)):
         bcm.append(transientSimulation.read_current_amplitude(time))
         time = time + bcm_resolution
--- simulation/main.py
+++ simulation/main.py
@@ -27,7 +27,7 @@ def set_time_resolution(interval): #sets the time resolution for the x-axis of t
         int_num_of_seconds = int_num_of_seconds + interval
     plt.xticks(tick_location , tick_label)
 
-#transientSimulation.calc_wave(lower_bound_limit_radian , upper_bound_limit_radian)
+transientSimulation.calc_wave(lower_bound_limit_radian , upper_bound_limit_radian)
 #plt.plot(storage)
 #set_time_resolution(graph_time_interval)
 #plt.show()
@@ -35,5 +35,8 @@ def set_time_resolution(interval): #sets the time resolution for the x-axis of t
 BCMSimulation.calc_bcm()
 plt.plot(bcm)
 plt.show()
+
+#print(len(storage))
+#print(translationLayer.num_of_seconds_bounded * translationLayer.bcm_sampling_rate)
 
 #not sure if set_time_resolution() is setting an accurate time scale
